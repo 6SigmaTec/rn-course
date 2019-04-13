41795de932cf58e4bf05e6d8fb5f4880d1840978
Andre Alves <andre.alves@sixsigma.com.br>
2019-04-13 04:11:40 -0300

    Criacao do projeto

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Sera que vai aparecer????/</Text>
+        <Text>Nova linha de texto</Text>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
